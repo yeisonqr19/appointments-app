9df0ab8bd29afe4abeb2d9ab351e7357fa121381
yeison <ingyeisonqr19@gmail.com>
2021-07-06 11:04:55 -0500

    state Create to control user Data

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+  const [cita, setCita] = useState({
+    pet: "",
+    owner: "",
+    dateDischarge: "",
+    time: "",
+    symptoms: "",
+  });
+
+  //Funcion que se ejecutara al detectar cambios en los inputs
+  const updateState = (e) => {
+    setCita({
+      ...cita,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  //Destructuring al State:
+  const { pet, owner, dateDischarge, time, symptoms } = cita;
+
   return (
     <>
       <h2>create an appointment </h2>
@@ -11,6 +30,8 @@ const Form = () => {
           name="pet"
           className="u-full-width"
           placeholder="pet Name"
+          onChange={updateState}
+          value={pet}
         />
 
         <label>owner's name</label>
@@ -19,16 +40,35 @@ const Form = () => {
           name="owner"
           placeholder="owner's name"
           className="u-full-width"
+          onChange={updateState}
+          value={owner}
         />
 
         <label>discharge date</label>
-        <input type="date" name="dateDischarge" className="u-full-width" />
+        <input
+          type="date"
+          name="dateDischarge"
+          className="u-full-width"
+          onChange={updateState}
+          value={dateDischarge}
+        />
 
         <label>time</label>
-        <input type="time" name="time" className="u-full-width" />
+        <input
+          type="time"
+          name="time"
+          className="u-full-width"
+          onChange={updateState}
+          value={time}
+        />
 
         <label>symptoms</label>
-        <textarea name="symptoms" className="u-full-width"></textarea>
+        <textarea
+          name="symptoms"
+          className="u-full-width"
+          onChange={updateState}
+          value={symptoms}
+        ></textarea>
 
         <button type="submit" className="u-full-width button-primary">
           add appointment
